d47af8e3a8a786cf513e86306989ec52399f8bb6
Jaemin Jo <jmjo@hcil.snu.ac.kr>
2014-08-19 15:03:05 +0900

    list view

---
+++ index.js
@@ -0,0 +1,35 @@
+var app = {};
+
+angular
+  .module('paperListApp', [])
+  .controller('PaperListCtrl', function($scope){
+    $scope.app = app;
+  });
+
+$(function(){
+  var count;
+  $.ajax({
+    url: 'data.json', 
+    dataType: 'json',
+    success: function(confList){
+      app.confList = confList;
+      count = confList.length;
+      confList.forEach(function(conf){
+        $.ajax({
+          url: conf.path,
+          dataType: 'json',
+          success: function(papers){
+            count--;
+            conf.papers = papers;
+
+            if(count == 0){
+              app.count = confList.map(function(c){return c.papers.length;}).reduce(function(a,b){return a+b;},0);
+              $('#paperList').scope().$apply();
+            }
+          }
+        });
+      });
+    }
+  });
+
+});
