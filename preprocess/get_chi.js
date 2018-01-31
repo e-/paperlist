19c50b43d3703f0bc1a64fd29307997785f830d3
Jaemin Jo <jmjo@hcil.snu.ac.kr>
2018-01-31 16:49:44 +0900

    added chi17

--- preprocess/get_chi.js
+++ preprocess/get_chi.js
@@ -1,6 +1,12 @@
+var script = document.createElement('script');
+  script.src = 'https://code.jquery.com/jquery-1.11.0.min.js';
+  script.type = 'text/javascript';
+  document.getElementsByTagName('head')[0].appendChild(script);
+
+
 (function(console){
 
-console.save = function(data, filename){
+  console.save = function(data, filename){
 
     if(!data) {
         console.error('Console.save: No data')
